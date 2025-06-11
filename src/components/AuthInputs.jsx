c85b3bc75f872b522467c50368f6f57daee67a01
Matthew Aitken <matt@itken.net>
2025-06-11 11:48:48 +0100

    Split styled components into files

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,34 +1,15 @@
 import { useState } from 'react';
 import { styled } from 'styled-components';
 
+import Button from './Button.jsx';
+import CustomInput from './Input.jsx';
+
 // Syntax for styled components 'tagged templates' use standard css code
 const ControlContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
   margin-bottom: 1.5rem;
-`
-
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  // conditional styling in styled-components
-  color: ${({ $invalid }) => $invalid ? '#f87181' : '#6b7280'}
-`
-
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: ${({ $invalid }) => $invalid ? '#fed2d2' : '#d1d5db'};
-  color: ${({ $invalid }) => $invalid ? '#ef4444' : '#374151'};
-  border: 1px solid ${({ $invalid }) => $invalid ? '#f73f3f' : 'transparent'};
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `
 
 export default function AuthInputs() {
@@ -55,31 +36,22 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       {/* Using a styled component just like a normal react component */}
       <ControlContainer>
-        <p>
-          {/* Using className conditionally also forwards props on styled components */}
-          {/* So you can also send onChange etc */}
-          {/* Using $ as a prefix for props stops clashing with built in props */}
-          <Label $invalid={emailNotValid}>Email</Label>
-          <Input
-            type="email"
-            $invalid={emailNotValid}
+          <CustomInput
+            label='Email'
+            invalid={emailNotValid}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
-        </p>
-        <p>
-          <Label $invalid={passwordNotValid}>Password</Label>
-          <Input
-            type="password"
-            $invalid={passwordNotValid}
+          <CustomInput
+            label='Password'
+            invalid={passwordNotValid}
             onChange={(event) => handleInputChange('password', event.target.value)}
           />
-        </p>
       </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
